ff7da3f2fcb783b7f48ecabecb3962799955ab74
unknown <kmckendall@gmail.com>
2017-11-14 23:20:18 -0500

    added a reset for the whole game, removed powerDown, and updated CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,89 +1,89 @@
-var powerUp = 0;
-var powerDown = 0;
-var totalCount = 0;
-var imageCrystal1 = $("#crystal1");
-var imageCrystal2 = $("#crystal2");
-var imageCrystal3 = $("#crystal3");
-var imageCrystal4 = $("#crystal4");
-var randomGuess = Math.floor(Math.random() * 102 + 19);
-var randomCrystal1 = Math.floor(Math.random() * 12 + 1);
-var randomCrystal2 = Math.floor(Math.random() * 12 + 1);
-var randomCrystal3 = Math.floor(Math.random() * 12 + 1);
-var randomCrystal4 = Math.floor(Math.random() * 12 + 1);
+// var powerUp = 0;
+// var powerDown = 0;
+// var totalCount = 0;
+// var imageCrystal1 = $("#crystal1");
+// var imageCrystal2 = $("#crystal2");
+// var imageCrystal3 = $("#crystal3");
+// var imageCrystal4 = $("#crystal4");
+// var randomGuess = Math.floor(Math.random() * 102 + 19);
+// var randomCrystal1 = Math.floor(Math.random() * 12 + 1);
+// var randomCrystal2 = Math.floor(Math.random() * 12 + 1);
+// var randomCrystal3 = Math.floor(Math.random() * 12 + 1);
+// var randomCrystal4 = Math.floor(Math.random() * 12 + 1);
 
-function startGame() {
+// function startGame() {
 
-    randomGuess;
-    randomCrystal1;
-    randomCrystal2;
-    randomCrystal3;
-    randomCrystal4;
+//     randomGuess;
+//     randomCrystal1;
+//     randomCrystal2;
+//     randomCrystal3;
+//     randomCrystal4;
 
-    console.log(randomCrystal1);
-    console.log(randomCrystal2);
-    console.log(randomCrystal3);
-    console.log(randomCrystal4);
-    console.log(randomGuess);
+//     console.log(randomCrystal1);
+//     console.log(randomCrystal2);
+//     console.log(randomCrystal3);
+//     console.log(randomCrystal4);
+//     console.log(randomGuess);
 
-    document.getElementById("powerup").textContent = powerUp;
-    document.getElementById("powerdown").textContent = powerDown;
-    document.getElementById("numbertoguess").textContent = randomGuess;
+//     document.getElementById("powerup").textContent = powerUp;
+//     document.getElementById("powerdown").textContent = powerDown;
+//     document.getElementById("numbertoguess").textContent = randomGuess;
 
-    playGame();
-};
+//     playGame();
+// };
 
 
-function playGame() {
-    $("#crystal1").on("click", function () {
-        imageCrystal1.attr("data-crystalvalue1", randomCrystal1);
-        $("#crystal1").append(randomCrystal1);
-        totalCount += randomCrystal1
-        document.getElementById("totalcount").textContent = totalCount
-        console.log(totalCount)
-        console.log(randomCrystal1);
-        winorlose();
-    });
+// function playGame() {
+//     $("#crystal1").on("click", function () {
+//         imageCrystal1.attr("data-crystalvalue1", randomCrystal1);
+//         $("#crystal1").append(randomCrystal1);
+//         totalCount += randomCrystal1
+//         document.getElementById("totalcount").textContent = totalCount
+//         console.log(totalCount)
+//         console.log(randomCrystal1);
+//         winorlose();
+//     });
 
-    $("#crystal2").on("click", function () {
-        imageCrystal2.attr("data-crystalvalue2", randomCrystal2);
-        $("#crystal2").append(imageCrystal2);
-        totalCount += randomCrystal2
-        console.log(randomCrystal2);
-        document.getElementById("totalcount").textContent = totalCount
-        winorlose();
-    });
+//     $("#crystal2").on("click", function () {
+//         imageCrystal2.attr("data-crystalvalue2", randomCrystal2);
+//         $("#crystal2").append(imageCrystal2);
+//         totalCount += randomCrystal2
+//         console.log(randomCrystal2);
+//         document.getElementById("totalcount").textContent = totalCount
+//         winorlose();
+//     });
 
-    $("#crystal3").on("click", function () {
-        imageCrystal3.attr("data-crystalvalue3", randomCrystal3);
-        $("#crystal3").append(imageCrystal3);
-        totalCount += randomCrystal3
-        document.getElementById("totalcount").textContent = totalCount
-        winorlose();
-    });
+//     $("#crystal3").on("click", function () {
+//         imageCrystal3.attr("data-crystalvalue3", randomCrystal3);
+//         $("#crystal3").append(imageCrystal3);
+//         totalCount += randomCrystal3
+//         document.getElementById("totalcount").textContent = totalCount
+//         winorlose();
+//     });
 
-    $("#crystal4").on("click", function () {
-        imageCrystal4.attr("data-crystalvalue4", randomCrystal4);
-        $("#crystal4").append(imageCrystal4);
-        totalCount += randomCrystal4
-        document.getElementById("totalcount").textContent = totalCount
-        winorlose();
-    });
-    console.log("yeah");
+//     $("#crystal4").on("click", function () {
+//         imageCrystal4.attr("data-crystalvalue4", randomCrystal4);
+//         $("#crystal4").append(imageCrystal4);
+//         totalCount += randomCrystal4
+//         document.getElementById("totalcount").textContent = totalCount
+//         winorlose();
+//     });
+//     console.log("yeah");
 
-};
-// playGame();
+// };
+// // playGame();
 
-function winorlose() {
-    console.log("yea");
-    if (totalCount === randomGuess) {
-        powerUp++;
-        document.getElementById("powerup").textContent = powerUp
-        console.log(powerUp);
+// function winorlose() {
+//     console.log("yea");
+//     if (totalCount === randomGuess) {
+//         powerUp++;
+//         document.getElementById("powerup").textContent = powerUp
+//         console.log(powerUp);
 
-    } else if (totalCount > randomGuess) {
-        powerDown++;
-        document.getElementById("powerdown").textContent = powerDown
-    };
-    startGame();
-};
-startGame();
+//     } else if (totalCount > randomGuess) {
+//         powerDown++;
+//         document.getElementById("powerdown").textContent = powerDown
+//     };
+//     startGame();
+// };
+// startGame();
